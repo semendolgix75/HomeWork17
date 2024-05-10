f6d16700a8f82d61e4d7904ba5f8e43e373d27d2
Semen <semen.dolgix@yandex.ru>
2024-05-10 17:59:12 +0500

    Correct after checking

--- src/Main.java
+++ src/Main.java
@@ -4,28 +4,26 @@ import exception.WrongLoginException;
 import exception.WrongPasswordException;
 
 
-
 public class Main {
-
 
     public static void main(String[] args) {
 
-        String login = "12345678902345678901";
+        String login = "12345678901234567890";
         System.out.println("Введен логин: " + login);
-
-        String password = "123456*";
+        String password = "123456";
         System.out.println("Введен пароль: " + password);
-        String confirmPassword = "123456*";
+        String confirmPassword = "123456";
         System.out.println("Повтор пароля: " + confirmPassword);
         try {
-            CheckLoginPassword.check(login,password,confirmPassword);
-        } catch (WrongLoginException e) {
+            CheckLoginPassword.check(login, password, confirmPassword);
+            System.out.println("Пароль соответствует требованиям");
+        } catch (WrongLoginException | WrongPasswordException e) {
             System.out.println(e.getMessage());
-
+        } finally {
+            System.out.println("Проверка пароля завершена");
         }
 
     }
-
 
     public static void checkLogin(String login) throws WrongLoginException {
         if (login.length() > 20) {
@@ -36,33 +34,8 @@ public class Main {
             throw new WrongLoginException(" ОШИБКА.Login должен содержать в себе только латинские буквы, цифры и знак подчеркивания.");
         }
         ;
-//        {
-//            if(it.current() < '0' | it.current() > '9' & it.current() < 'A' |
-//                    it.current() > 'Z' & it.current() < 'a' & it.current() != '_' | it.current() > 'z'){
-//                throw new WrongLoginException();
-//            }
-//            it.next();
-//        }
     }
 
-    public static void checkPasswords(String password, String confirmPassword) throws WrongPasswordException {
 
-        if (!password.equals(confirmPassword)) {
-            throw new WrongPasswordException(" ОШИБКА. Пароли  должны быть равны.");
-        }
-
-        if (password.length() >= 20) {
-            throw new WrongPasswordException(" ОШИБКА. Пароль длиннее 20 символов");
-        }
-
-        char[] character = password.toCharArray();
-        for (char ch : character) {
-            if (ch < '0' | ch > '9' & ch < 'A' |
-                    ch > 'Z' & ch < 'a' & ch != '_' | ch > 'z') {
-                throw new WrongPasswordException();
-            }
-        }
-
-    }
 }
 
--- src/exception/WrongPasswordException.java
+++ src/exception/WrongPasswordException.java
@@ -2,11 +2,6 @@ package exception;
 
 public class WrongPasswordException extends Exception{
 
-
-    public WrongPasswordException() {
-        super(" ОШИБКА.Login должен содержать в себе только латинские буквы, цифры и знак подчеркивания.");
-    }
-
     public WrongPasswordException(String message) {
         super(message);
     }
